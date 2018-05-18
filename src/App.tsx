a94ca4d698ff4317c1605a2b2f71e471260f53db
Umar <faisal.exalted@outlook.com>
2018-05-18 15:12:54 +0500

    type definitions for alert, button, message and tooltip plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import { Button } from 'rsuite';
-import { Alert } from 'rsuite-notification';
+import { Alert, Button, Message } from 'rsuite';
+// import {Modal} from 'rsuite';
 import './App.css';
 import logo from './logo.svg';
 
 import 'rsuite/dist/styles/rsuite.min.css';
-// import {Button, Notification} from 'rsuite';
-// import {Alert, Notify} from 'rsuite-notification';
 
-
+const msgStyle = {
+  margin: '3%'
+};
 class App extends React.Component {
+  public handleClick = () => {
+    Alert.info('this is test message');
+  };
+
   public render() {
     return (
       <div className="App">
@@ -20,10 +24,16 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Button appearance="primary" onClick={Alert.info('this is info message')}> Hello world </Button>
+        <h3>Button and Alert plugin: </h3>
+        <Button appearance="primary" onClick={this.handleClick}> Hello world </Button>
+        <h3>Message plugin: </h3>
+        <div style={msgStyle}>
+          <Message type="success" title="test success" description="success message here" />        
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
